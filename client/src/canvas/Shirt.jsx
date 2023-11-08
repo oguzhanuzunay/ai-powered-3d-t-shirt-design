0c20cde6eea2f30cffc0f05c7b5b03473dd2486f
Oguzhan <oguzhanzny@gmail.com>
2023-11-08 21:44:31 +0300

    Model import change

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-import { easing } from "maath";
-import { useSnapshot } from "valtio";
-import { useFrame } from "@react-three/fiber";
-import { Decal, useGLTF, useTexture } from "@react-three/drei";
+import React, { Suspense } from 'react'
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
-import state from "../store";
+import state from '../store';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+  const { nodes, materials } = useGLTF('/3D-Tshirt-Design/shirt_baked.glb');
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
   useFrame((state, delta) =>
-    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
-  );
+    easing.dampC(
+      materials.lambert1.color,
+      snap.color,
+      0.25,
+      delta));
 
   const stateString = JSON.stringify(snap);
+
 
   return (
     <group key={stateString}>
@@ -28,29 +32,30 @@ const Shirt = () => {
         material-roughness={1}
         dispose={null}
       >
-        {snap.isFullTexture && (
+        {snap.isFullTexture &&
           <Decal
-            position={[0, 0, 0]}
+            position={[0, 0.0, 0]}
             rotation={[0, 0, 0]}
             scale={1}
             map={fullTexture}
-          />
-        )}
+          />}
 
-        {snap.isLogoTexture && (
+        {snap.isLogoTexture &&
           <Decal
             position={[0, 0.04, 0.15]}
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            map-anisotropy={16}
+            //map-anisotropy={16}
             depthTest={false}
             depthWrite={true}
-          />
-        )}
+          />}
+
+
       </mesh>
     </group>
-  );
-};
 
-export default Shirt;
+  )
+}
+
+export default Shirt
