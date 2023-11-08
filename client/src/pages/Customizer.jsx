1a4931fcf2eca3cdae39a307be3d2b91a58e5111
Oguzhan <oguzhanzny@gmail.com>
2023-11-08 19:08:56 +0300

    fix client deployment

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -61,7 +61,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://project-threejs-ai-72c1.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/AI-Powered-3D-Tshirt-Design-WebApp/',
   plugins: [react()],
 })
