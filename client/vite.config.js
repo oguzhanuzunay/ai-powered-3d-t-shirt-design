07e00346368823574156b0ebf534b51ee6164b55
Oguzhan <oguzhanzny@gmail.com>
2023-11-09 00:38:12 +0300

    Model import change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projects/ai-powered-3d-t-shirt-design/",
+  base: "/ai-powered-3d-t-shirt-design/",
 });
