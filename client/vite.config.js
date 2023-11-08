f5838b2b8c195c747abdfa2bb317a35237308246
Oguzhan <oguzhanzny@gmail.com>
2023-11-08 19:31:12 +0300

    client deployment fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AI-Powered-3D-Tshirt-Design-WebApp/",
+  base: "/3D-Tshirt-Design/",
 });
